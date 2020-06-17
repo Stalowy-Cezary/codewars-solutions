dcce934c102feea7244771ddfd50f1396462faa0
Stalowy-Cezary <63396078+Stalowy-Cezary@users.noreply.github.com>
2020-06-18 00:37:30 +0200

    Update Extract the domain name from a URL.py

--- Extract the domain name from a URL.py
+++ Extract the domain name from a URL.py
@@ -1,1 +1,7 @@
-
+def domain_name(url):
+    if "www" in url:
+        return url.split(".")[1].split(".")[-1]
+    elif "http" in url:
+        return url.split("//")[-1].split(".")[0]
+    else:
+        return url.split(".")[0].split(".")[0]
